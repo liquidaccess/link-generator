0883a15a83fe14a8771c03b14f7f2492c9a4fe93
mike <mb@liquidaccess.com>
2022-12-06 16:15:11 +0200

    Remove useless comments

--- index.js
+++ index.js
@@ -48,7 +48,6 @@ var mintLink = `${laUrl}/mint?user_id=${user_id}&expiration_date=${expiration_da
 // var mintLink = `${laUrl}/mint?user_id=${user_id}&subscription_id=${subscription_id}&expiration_date=${expiration_date}&merchant_id=${merchant_id}&timestamp=${timestamp}&signature=${mintSignature}`;
 
 console.log('URL for minting:', mintLink);
-// https://uat.liquid-access.rocks/mint?user_id=mb@liquidaccess.com&subscription_id=1&expiration_date=1665240000&merchant_id=1&timestamp=1665140000&signature=632145ef71d2f795be0f43acd18422141a1a8feca30791d0ad693234979eb484
 
 /* *** activate */
 
@@ -62,4 +61,3 @@ var activateSignature = sha256.hmac(key, activateLine);
 var activateLink = `${laUrl}/activate?user_id=${user_id}&expiration_date=${expiration_date}&merchant_id=${merchant_id}&timestamp=${timestamp}&signature=${activateSignature}`;
 
 console.log('URL for activation:', activateLink);
-// https://uat.liquid-access.rocks/activate?user_id=mb@liquidaccess.com&expiration_date=1665240000&merchant_id=1&timestamp=1665140000&signature=4ee5b393228fe7f16e10ccdc43b303d02a9cdd97612c971ac39fff4049858d22
